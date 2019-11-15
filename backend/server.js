7f87a40f096261e1c2e76438fe1e8aecd2945224
jandoso <carol.jandoso@gmail.com>
2019-11-14 21:46:07 -0300

    estrutura mvc, servidor e app

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const app = require('./src/app');
+const port = 8080;
+
+app.listen(port, (err) => {
+    if(err){
+        console.log("Houve um erro ao iniciar o servidor");
+    } else{
+        console.log(`O servidor está rodando na porta ${port}`);
+    };
+});
