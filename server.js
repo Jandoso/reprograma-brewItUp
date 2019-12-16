51770dd064c767f4fed0dd4341106a8575896ca0
Jandoso <carol.jandoso@gmail.com>
2019-12-16 00:01:37 -0200

    mudança para deploy

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require('./src/app');
-const port = 8080;
+const port = 8080 || process.env.PORT;
 
-app.listen(port || process.env.PORT , (err) => {
+app.listen(port , (err) => {
     if(err){
         console.log("Houve um erro ao iniciar o servidor");
     } else{
